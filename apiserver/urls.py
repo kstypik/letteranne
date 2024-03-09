6c41f3521c51502bdca3b6909fc026d5b4f72b96
Kacper Stypik <53559764+kstypik@users.noreply.github.com>
2024-03-09 21:51:15 +0100

    Split API URLs to separate namespace

--- apiserver/urls.py
+++ apiserver/urls.py
@@ -8,22 +8,26 @@ from drf_spectacular.views import (
     SpectacularSwaggerView,
 )
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api-auth/", include("rest_framework.urls")),
-    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+api_urlpatterns = [
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "api/schema/swagger-ui/",
+        "schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
         name="swagger-ui",
     ),
     path(
-        "api/schema/redoc/",
+        "schema/redoc/",
         SpectacularRedocView.as_view(url_name="schema"),
         name="redoc",
     ),
     path("accounts/", include("dj_rest_auth.urls")),
     path("accounts/signup/", include("dj_rest_auth.registration.urls")),
+]
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api-auth/", include("rest_framework.urls")),
+    path("api/", include(api_urlpatterns)),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
 
