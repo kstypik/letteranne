cf1b8cb2fa5761eeccc1369a8805dc211610ccf8
Kacper Stypik <53559764+kstypik@users.noreply.github.com>
2024-03-09 21:51:50 +0100

    Handle statics in development

--- apiserver/urls.py
+++ apiserver/urls.py
@@ -28,8 +28,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/", include(api_urlpatterns)),
-    *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
