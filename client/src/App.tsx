c2943a369172dcaa8ffc5ceaa97fda3f1ca20b25
Kacper Stypik <53559764+kstypik@users.noreply.github.com>
2024-03-11 22:20:35 +0100

    Move from Remix to Vite+React

---
+++ client/src/App.tsx
@@ -0,0 +1,12 @@
+import { MantineProvider } from "@mantine/core";
+import "@mantine/core/styles.css";
+
+function App() {
+	return (
+		<MantineProvider>
+			<p>Test</p>
+		</MantineProvider>
+	);
+}
+
+export default App;
