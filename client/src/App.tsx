df3319f1030cbc44bce07a2ddbce16f8159a9b18
Kacper Stypik <53559764+kstypik@users.noreply.github.com>
2024-03-15 20:16:18 +0100

    Add RTK Query

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,40 @@
 import { MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { Provider } from "react-redux";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Header } from "./components/Header";
 import { SignUpPage } from "./pages/SignUp";
+import { store } from "./store";
 
 const router = createBrowserRouter([
-	{
-		id: "root",
-		path: "/",
-		Component: Layout,
-	},
-	{
-		id: "auth",
-		path: "/signup",
-		Component: SignUpPage,
-	},
+  {
+    id: "root",
+    path: "/",
+    Component: Layout,
+  },
+  {
+    id: "auth",
+    path: "/signup",
+    Component: SignUpPage,
+  },
 ]);
 
 function Layout() {
-	return (
-		<>
-			<Header />
-		</>
-	);
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 function App() {
-	return (
-		<MantineProvider>
-			<RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
-		</MantineProvider>
-	);
+  return (
+    <Provider store={store}>
+      <MantineProvider>
+        <RouterProvider router={router} fallbackElement={<p>Loading...</p>} />
+      </MantineProvider>
+    </Provider>
+  );
 }
 
 export default App;
