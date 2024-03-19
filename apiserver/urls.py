8953e0302a995a589c1f22ca314dcd7d3dd06867
Kacper Stypik <53559764+kstypik@users.noreply.github.com>
2024-03-19 21:07:51 +0100

    Fix mypy giving wrong suggestion

--- apiserver/urls.py
+++ apiserver/urls.py
@@ -27,7 +27,7 @@ api_urlpatterns = [
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
-    path("api/", include("api_urlpatterns")),
+    path("api/", include(api_urlpatterns)),  # type: ignore[arg-type]
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
