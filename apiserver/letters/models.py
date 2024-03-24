1473c679e33ae9fb8e63575533cc26ceaae4d862
Kacper Stypik <53559764+kstypik@users.noreply.github.com>
2024-03-24 20:23:58 +0100

    Add minimal Letter model

--- apiserver/letters/models.py
+++ apiserver/letters/models.py
@@ -1,3 +1,29 @@
+from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from common.models import BaseModel
+
+
+class Letter(BaseModel):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=_("author"),
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="written_letters",
+    )
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        verbose_name=("recipient"),
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name="received_letters",
+    )
+    title = models.CharField(_("title"), max_length=100)
+    body = models.TextField(_("body"))
+    scheduled_at = models.DateTimeField(_("scheduled at"))
+    delivered_at = models.DateTimeField("delivered at", null=True, blank=True)
+
+    def __str__(self):
+        return self.title
